b3db4cfd4200ff07b3a620ab85e138930a43975d
Hridayesh Sharma <vyasriday7@gmail.com>
2019-06-25 22:25:14 +0530

    added eslint, prettier and parcel to the project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-const Pet = ({name, animal, breed}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
-    React.createElement("h2",   {}, animal),
+    React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed)
-  ])
-}
+  ]);
+};
 
 const App = () => {
-  return (
-    React.createElement("div", {},
-    [
-      React.createElement("h1", {} , "Adopt Me"),
-      React.createElement(Pet, {
-        name: 'Trace', 
-        animal:'Dog', 
-        breed: 'German',  
-      }),
-      React.createElement(Pet, {
-        name: 'Bernard', 
-        animal:'Cat', 
-        breed: 'Spanish',  
-      }),
-      React.createElement(Pet, {
-        name: 'Pertcy', 
-        animal:'Parrot', 
-        breed: 'Helvetica',  
-      })
-    ]
-    )
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Trace",
+      animal: "Dog",
+      breed: "German"
+    }),
+    React.createElement(Pet, {
+      name: "Bernard",
+      animal: "Cat",
+      breed: "Spanish"
+    }),
+    React.createElement(Pet, {
+      name: "Pertcy",
+      animal: "Parrot",
+      breed: "Helvetica"
+    })
+  ]);
+};
 
-ReactDOM.render( React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
