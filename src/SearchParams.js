9a38a06dda39f192a02cb844c5d98f3802dadff8
Hridayesh Sharma <vyasriday7@gmail.com>
2019-06-27 19:52:21 +0530

    created a custom hook for dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
+
 const SearchParmas = () => {
   /*
    * useState takes the default state as argument and returns an array which contains the
@@ -8,9 +10,9 @@ const SearchParmas = () => {
    * const arr = useState("initial State") // ["initial State", function]
    */
   const [location, setLocation] = useState('Seattle, WA');
-  const [animal, setAnimal] = useState('dog');
-  const [breed, setBreed] = useState('');
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breeds, setBreeds] = useState([]);
+  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
 
   return (
     <div className="search-params">
@@ -25,39 +27,8 @@ const SearchParmas = () => {
             onChange={event => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={event => setAnimal(event.target.value)}
-            onBlur={event => setAnimal(event.target.value)}
-          >
-            <option />
-            {ANIMALS.map((animal, index) => (
-              <option value={animal} key={index}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={event => setBreed(event.target.value)}
-            onBlur={event => setBreed(event.target.value)}
-            disabled={!breeds.length}
-          >
-            <option>All</option>
-            {breeds.map(breed => (
-              <option key value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button type="submit">Submit</button>
       </form>
     </div>
---
+++ src/useDropdown.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(' ', '').toLowerCase()}`;
+  const Dropdown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={event => setState(event.target.value)}
+        onBlur={event => setState(event.target.value)}
+        disabled={!options.length}
+      >
+        <option />
+        {options.map(option => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [state, Dropdown, setState];
+};
+
+export default useDropdown;
