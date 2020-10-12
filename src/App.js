c01a871db19f46e7cb57306674edcf10bfa41a36
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-10-12 20:02:55 +0530

    added eslint and prettier configs

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const createElement = React.createElement;
+
+const Pet = (props) => {
+  const { name, animal, breed } = props;
+  return createElement("div", {}, [
+    createElement("h1", {}, name),
+    createElement("h2", {}, animal),
+    createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Stray",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
