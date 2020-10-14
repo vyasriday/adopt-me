342c677f248b6dc1d9b5b37c955b2ae85fa9f244
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-10-14 18:53:19 +0530

    added hooks and custom hook

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,17 @@
-import { createElement } from "react";
+import React from "react";
 
 export default function Pet(props) {
   const { name, animal, breed } = props;
-  return createElement("div", {}, [
-    createElement("h1", {}, name),
-    createElement("h2", {}, animal),
-    createElement("h2", {}, breed),
-  ]);
+  // return createElement("div", {}, [
+  //   createElement("h1", {}, name),
+  //   createElement("h2", {}, animal),
+  //   createElement("h2", {}, breed),
+  // ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
---
+++ src/SearchParams.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  // these are custom hooks and therefore they will be fired on rerender and are not governed by useState dependency rules
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    });
+  }, [animal, setBreeds, setBreed]);
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
