9a4ede205858984d624e5acd6f2a95e7c697b9f0
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-10-16 23:09:19 +0530

    added modal component using react portals

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { navigate } from '@reach/router';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
 import ThemeContext from './ThemeContext';
+import Modal from './Modal';
 // reach router will automatically pass the id param to this component as props
 class Details extends Component {
 	state = {
 		loading: true,
+		showModal: false,
 	};
 	componentDidMount() {
 		pet.animal(this.props.id).then(({ animal }) => {
 			this.setState({
+				url: animal.url,
 				name: animal.name,
 				animal: animal.type,
 				location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -22,12 +26,22 @@ class Details extends Component {
 		});
 	}
 
+	toggleModal = () => this.setState({ showModal: !this.state.showModal });
+	adopt = () => navigate(this.state.url);
 	render() {
 		if (this.state.loading) {
 			return <h1>Loading ...</h1>;
 		}
 
-		const { animal, breed, location, description, name, media } = this.state;
+		const {
+			animal,
+			breed,
+			location,
+			description,
+			name,
+			media,
+			showModal,
+		} = this.state;
 		return (
 			<div className='details'>
 				<Carousel media={media} />
@@ -36,12 +50,24 @@ class Details extends Component {
 				<ThemeContext.Consumer>
 					{/* themeHook is the value passed down from Provider in App component */}
 					{(themeHook) => (
-						<button style={{ backgroundColor: themeHook[0] }}>
+						<button
+							style={{ backgroundColor: themeHook[0] }}
+							onClick={this.toggleModal}
+						>
 							Adopt {name}
 						</button>
 					)}
 				</ThemeContext.Consumer>
 				<p>{description}</p>
+				{showModal ? (
+					<Modal>
+						<h1>Would you like to adopt {name}</h1>
+						<div className='buttons'>
+							<button onClick={this.adopt}>Yes</button>
+							<button>No, I am a monster</button>
+						</div>
+					</Modal>
+				) : null}
 			</div>
 		);
 	}
